2b1e784642635eb542328e1de14b134ca0527b12
sachin-vanveru <sachinvanveru55@gmail.com>
2016-11-24 10:41:52 +0530

    ui of prjct

--- SachinCakeShop/src/main/java/com/niit/dao/ProductDaoImple.java
+++ SachinCakeShop/src/main/java/com/niit/dao/ProductDaoImple.java
@@ -9,9 +9,21 @@ public class ProductDaoImple implements ProductDao {
 		prdlist.add(new Product(101, "Sachin", 700, 10, "Choclate cake with vanila topping", "cake.jpg"));
 		prdlist.add(new Product(102, "sarayana", 750, 10, "anniversary cake with micky mouse designe", "ann3.jpg"));
 		prdlist.add(new Product(103, "sahal", 850, 10,"birthday cake with Strawberry" , "cake2.jpg"));
-		prdlist.add(new Product(103, "Ashwini", 550, 10,"birthday cake" , "cake5.jpg"));
-		prdlist.add(new Product(103, "Manali", 900, 10,"birthday cake with chocolate" , "birthday.jpg"));
-		prdlist.add(new Product(103, "pratibha", 1850, 10,"Anniversary cake with photo designe" , "ann2.jpg"));
+		prdlist.add(new Product(104, "Ashwini", 550, 10,"birthday cake" , "cake5.jpg"));
+		prdlist.add(new Product(105, "Manali", 900, 10,"birthday cake with chocolate" , "birthday.jpg"));
+		prdlist.add(new Product(106, "pratibha", 1850, 10,"Anniversary cake with photo designe" , "ann2.jpg"));
+		prdlist.add(new Product(107, "pratibha", 1850, 10,"Anniversary cake with photo designe" , "ann4.jpg"));
+		
+		prdlist.add(new Product(108, "pratibha", 1850, 10,"Anniversary cake with photo designe" , "ann2.jpg"));
+		prdlist.add(new Product(109, "Ashwini", 550, 10,"birthday cake" , "cake5.jpg"));
+		prdlist.add(new Product(110, "Ashwini", 550, 10,"birthday cake" , "cake5.jpg"));
+		prdlist.add(new Product(111, "Ashwini", 550, 10,"birthday cake" , "cake5.jpg"));
+		prdlist.add(new Product(112, "Ashwini", 550, 10,"birthday cake" , "cake5.jpg"));
+		prdlist.add(new Product(113, "sahal", 850, 10,"birthday cake with Strawberry" , "cake2.jpg"));
+		prdlist.add(new Product(114, "sahal", 850, 10,"birthday cake with Strawberry" , "cake2.jpg"));
+		prdlist.add(new Product(115, "Sachin", 700, 10, "Choclate cake with vanila topping", "cake.jpg"));
+		prdlist.add(new Product(116, "Sachin", 700, 10, "Choclate cake with vanila topping", "cake.jpg"));
+		prdlist.add(new Product(117, "Sachin", 700, 10, "Choclate cake with vanila topping", "cake.jpg"));
 	}
 	public	List<Product>getAllProduct()
 	{
